06b826a1ef1fa63c0f9c3b49c75aa7a41af9909e
༄ᶦᶰᵈ᭄Sᴘʏ✿Gᴀᴍᴇʀ࿐ <gamingwithmayuresh7t@gmail.com>
2025-05-22 00:44:17 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,14 +4,14 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-API_ID = int(getenv("API_ID", 27353035))
-API_HASH = getenv("API_HASH", "cf2a75861140ceb746c7796e07cbde9e")
-OWNER_ID = int(getenv("OWNER_ID", "6752444356"))
-STRING_SESSION = getenv("STRING_SESSION", None)
-MONGO_URL = getenv("MONGO_URL", None)
-SUPPORT_GRP = getenv("SUPPORT_GRP", "BRANDED_WORLD")
-UPDATE_CHNL = getenv("UPDATE_CHNL", "BRANDED_PAID_CC")
-OWNER_USERNAME = getenv("OWNER_USERNAME", "BRANDEDKING82")
+API_ID = int(getenv("API_ID", 21907528))
+API_HASH = getenv("API_HASH", "c72220e5fc935797399d0a530d80ad8e")
+OWNER_ID = int(getenv("OWNER_ID", "7290350162"))
+STRING_SESSION = getenv("STRING_SESSION", "BQFOSEgAlMHFqRsFC5Jjr_mn0dYy1Ds_0TamfWXHJgrdtWKjT3vMFHIkVmu50YcxeXOsiVD_u4oXvEWwhqOmW0d9AMuDiKRmxOaV9Yi0pCkj9xYArqCf7WT6eByHGrupVm5Gds1Qk3ftnmk7gViRJe_qrtq_zTWKXlJrDy3e1kvu2ssc6mzjCrwDFdb0agk_0nY4n44gZR6w3Kk9kPxEZT4zvyhKIIfH2RfLPf0R4eh9HQ0BARzgpD_InmN-yHTld4h9idfMzyj7Qm8amwj-fjCk_eeMm0wx9z37AUHQRvzRu5pDPksZi1LzNcjwuL8N5Kc57PJ707TXVVFNhRHcppA6kAG4WwAAAAHSq1hVAA")
+MONGO_URL = getenv("MONGO_URL", "mongodb+srv://botbaby:botbaby@cluster0.zngip1b.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+SUPPORT_GRP = getenv("SUPPORT_GRP", "infinitygx_bot_support")
+UPDATE_CHNL = getenv("UPDATE_CHNL", "gxinfinity_support")
+OWNER_USERNAME = getenv("OWNER_USERNAME", "Best_Infinity_chatter_bot")
 
 # Random Start Images
 IMG = [
